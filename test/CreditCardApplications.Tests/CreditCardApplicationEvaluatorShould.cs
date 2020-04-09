4000829699b6126379847d510ed6581da1ebd915
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-04-09 10:39:58 +0200

    Implemented initial tests.

--- test/CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ test/CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -1,4 +1,5 @@
 using System;
+using CreditCardApplications.Console;
 using Xunit;
 
 namespace CreditCardApplications.Tests
@@ -6,9 +7,25 @@ namespace CreditCardApplications.Tests
     public class CreditCardApplicationEvaluatorShould
     {
         [Fact]
-        public void Test1()
+        public void AcceptHighIncomeApplications()
         {
+            var sut = new CreditCardApplicationEvaluator();
+            var crediCardApplication = new CreditCardApplication { GrossAnnualIncome = 20_000 };
 
+            var expected = sut.Evaluate(crediCardApplication);
+
+            Assert.Equal(CreditCardApplicationDecision.AutoAccepted, expected);
+        }
+
+        [Fact]
+        public void ReferYoungApplications()
+        {
+            var sut = new CreditCardApplicationEvaluator();
+            var crediCardApplication = new CreditCardApplication { Age = 19 };
+
+            var expected = sut.Evaluate(crediCardApplication);
+
+            Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, expected);
         }
     }
 }
