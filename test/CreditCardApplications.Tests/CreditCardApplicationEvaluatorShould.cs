190886c4ff58808bf991fb446c4bf044a642da00
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-04-11 08:58:56 +0200

    Added test with mock with a parameter value and return type.

--- test/CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ test/CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -32,5 +32,25 @@ namespace CreditCardApplications.Tests
 
             Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, expected);
         }
+
+        [Fact]
+        public void DeclineLowIncomeApplications()
+        {
+            Mock<IFrequentFlyerNumberValidator> mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+            // Adding argument and return value == true to the method.
+            mockValidator.Setup(x => x.IsValid("x")).Returns(true);
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+            var crediCardApplication = new CreditCardApplication
+            {
+                Age = 50,
+                GrossAnnualIncome = 19_999,
+                FrequentFlyerNumber = "x" //Must add the same value as given as paramenter in the mock object.
+            };
+
+            var expected = sut.Evaluate(crediCardApplication);
+
+            Assert.Equal(CreditCardApplicationDecision.AutoDeclined, expected);
+        }
     }
 }
